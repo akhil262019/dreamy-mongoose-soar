8adf4b05f0b7d9b85a1af88afaaa13edeed31f75
[scalix] <git@scalix.world>
2026-01-19 20:47:19 +0530

    [scalix] wrote 2 file(s)

---
+++ backend/app/extensions.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+# from flask_login import LoginManager
+# from flask_marshmallow import Marshmallow
+# from flask_cors import CORS
+
+db = SQLAlchemy()
+# login_manager = LoginManager()
+# marshmallow = Marshmallow()
+# cors = CORS()
+
+# Register extensions in __init__.py
